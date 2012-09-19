d91bfdb4efbd82874f0fbc57ec66540ac760fa7f
rguess <502876941@qq.com>
2012-09-19 09:14:22 +0800

    work

--- src/main/webapp/js/weather.js
+++ src/main/webapp/js/weather.js
@@ -5,7 +5,13 @@ function getWeather() {
 		url : 'rest/weather/get',
 		type : 'GET',
 		success : function(meta) {
-			alert(meta.weatherinfo.city);
+			var w = meta.weatherinfo;
+			$("#today").empty().append("今天:"+w.week+" "+w.temp1+" "+w.weather1);
+			$("#tomororw").empty().append("明天:"+parseInt(w.week)+1+" "+w.temp2+" "+w.weather2);
+			$("#w1").empty().append("明天:"+parseInt(w.week)+1+" "+w.temp2+" "+w.weather2);
+			$("#w2").empty().append("明天:"+parseInt(w.week)+1+" "+w.temp2+" "+w.weather2);
+			$("#w3").empty().append("明天:"+parseInt(w.week)+1+" "+w.temp2+" "+w.weather2);
+			$("#w4").empty().append("明天:"+parseInt(w.week)+1+" "+w.temp2+" "+w.weather2);
 		}
 	});
 }
